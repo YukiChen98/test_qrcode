678af46a22a1416ee0aae8650902c01fc70698bf
YukiChen98 <94729609+YukiChen98@users.noreply.github.com>
2023-04-30 18:58:22 -0400

    Update html5-qrcode-demo.js

--- html5-qrcode-demo.js
+++ html5-qrcode-demo.js
@@ -1,3 +1,7 @@
+var lastResult = ""
+var resultCount = 0
+
+
 function docReady(fn) {
     // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
@@ -8,28 +12,26 @@ function docReady(fn) {
     }
 } 
 
+function onScanSuccess(decodedText, decodedResult) {
+    if (decodedText !== lastResult) {
+        ++resultCount;
+        lastResult = decodedText;
+        console.log(`Scan result = ${decodedText}`, decodedResult);
+ 
+        resultContainer.innerHTML += `<div>[${resultCount}] - ${decodedText}</div>`;
+    }
+}
+
+function onScanError(qrCodeError) {
+    // This callback would be called in case of qr code scan error or setup error.
+    // You can avoid this callback completely, as it can be very verbose in nature.
+}
+
 docReady(function() {
     var resultContainer = document.getElementById('qr-reader-results');
-    var lastResult, countResults = 0;
     
     var html5QrcodeScanner = new Html5QrcodeScanner(
         "qr-reader", { fps: 10 });
-    
-    function onScanSuccess(decodedText, decodedResult) {
-        if (decodedText !== lastResult) {
-            ++countResults;
-            lastResult = decodedText;
-            console.log(`Scan result = ${decodedText}`, decodedResult);
- 
-            resultContainer.innerHTML += `<div>[${countResults}] - ${decodedText}</div>`;
-        }
-    }
-    
-    // Optional callback for error, can be ignored.
-    function onScanError(qrCodeError) {
-        // This callback would be called in case of qr code scan error or setup error.
-        // You can avoid this callback completely, as it can be very verbose in nature.
-    }
-    
+   
     html5QrcodeScanner.render(onScanSuccess, onScanError);
 });
